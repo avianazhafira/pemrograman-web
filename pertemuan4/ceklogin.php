7d307137a2281e4a85178bfc0222e9aeda15a775
avianazhafira <avianazhafira@gmail.com>
2020-10-06 12:49:57 +0700

    perubahan1

---
+++ pertemuan4/ceklogin.php
@@ -0,0 +1,19 @@
+<?php
+
+if (isset($_POST['tombolSubmit'])){
+    $username= $_POST['username'];
+    $password= $_POST['password'];
+
+    if($username=="anto"&&$password=="123"){
+        echo "Sukses";
+    }
+    else{
+        echo "Username/Password ada yang salah";
+    }
+}
+else{
+    echo "Mohon maaf cek login tidak bs diakses langsung";
+}
+
+
+?>
